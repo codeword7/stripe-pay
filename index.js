28bbaade570ffc5948e0bf41335d7a29c71c908b
Neeraj Yadav <ny29599@gmail.com>
2021-04-24 20:36:22 +0530

    stripe pay

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors({
+  origin: true
+}));
+app.use(express.json());
+const stripe = require('stripe')('sk_test_iPKB77psH3YICxlLnZ7ykhEp')
+
+app.post('/create-checkout-session', async (req, res) => {
+  const payment_data = req.body;
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: payment_data,
+    mode: 'payment',
+    success_url: 'http://localhost:3000/orderSuccess',
+    cancel_url: 'http://localhost:3000',
+  });
+  res.json({ id: session.id });
+});
+
+app.listen(4242, () => console.log(`Listening on port ${4242}!`));
